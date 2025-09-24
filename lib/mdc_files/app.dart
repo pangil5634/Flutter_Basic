c582988410b6fd906088ddc605298cbdd0c76fb7
pangil5634 <oksk6681@naver.com>
2025-09-24 17:33:35 +0900

    chore : mdc 104

--- lib/mdc_files/app.dart
+++ lib/mdc_files/app.dart
@@ -14,15 +14,30 @@
 
 import 'package:flutter/material.dart';
 
+import 'backdrop.dart';
+import 'category_menu_page.dart';
 import 'home.dart';
 import 'login.dart';
 import 'colors.dart';
-
+import 'model/product.dart';
 import 'supplemental/cut_corners_border.dart';
 
 // TODO: Convert ShrineApp to stateful widget (104)
-class ShrineApp extends StatelessWidget {
+class ShrineApp extends StatefulWidget {
   const ShrineApp({Key? key}) : super(key: key);
+
+  @override
+  State<ShrineApp> createState() => _ShrineAppState();
+}
+
+class _ShrineAppState extends State<ShrineApp> {
+  Category _currentCategory = Category.all;
+
+  void _onCategoryTap(Category category) {
+    setState(() {
+      _currentCategory = category;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +47,20 @@ class ShrineApp extends StatelessWidget {
       routes: {
         '/login': (BuildContext context) => const LoginPage(),
         // TODO: Change to a Backdrop with a HomePage frontLayer (104)
-        '/': (BuildContext context) => const HomePage(),
+        // TODO: Change to a Backdrop with a HomePage frontLayer (104)
+        '/': (BuildContext context) => Backdrop(
+              // TODO: Make currentCategory field take _currentCategory (104)
+              currentCategory: _currentCategory,
+              // TODO: Pass _currentCategory for frontLayer (104)
+              frontLayer: HomePage(category: _currentCategory),
+              // TODO: Change backLayer field value to CategoryMenuPage (104)
+              backLayer: CategoryMenuPage(
+                currentCategory: _currentCategory,
+                onCategoryTap: _onCategoryTap,
+              ),
+              frontTitle: Text('SHRINE'),
+              backTitle: Text('MENU'),
+            ),
         // TODO: Make currentCategory field take _currentCategory (104)
         // TODO: Pass _currentCategory for frontLayer (104)
         // TODO: Change backLayer field value to CategoryMenuPage (104)
@@ -49,25 +77,35 @@ final ThemeData _kShrineTheme = _buildShrineTheme();
 ThemeData _buildShrineTheme() {
   final ThemeData base = ThemeData.light();
   return base.copyWith(
+    appBarTheme: AppBarTheme(
+      backgroundColor: kShrinePink100, // 원하는 색상
+      foregroundColor: kShrineBrown900, // 글자/아이콘 색상
+    ),
     colorScheme: base.colorScheme.copyWith(
-      primary: kShrinePurple,
-      secondary: kShrinePurple,
+      primary: kShrinePink100,
+      onPrimary: kShrineBrown900,
+      secondary: kShrineBrown900,
       error: kShrineErrorRed,
     ),
-    scaffoldBackgroundColor: kShrineSurfaceWhite,
+    // TODO: Add the text themes (103)
+    textTheme: _buildShrineTextTheme(base.textTheme),
     textSelectionTheme: const TextSelectionThemeData(
-      selectionColor: kShrinePurple,
+      selectionColor: kShrinePink100,
     ),
+    // TODO: Add the icon themes (103)
+    // TODO: Decorate the inputs (103)
+    // TODO: Decorate the inputs (103)
+    // TODO: Decorate the inputs (103)
     inputDecorationTheme: const InputDecorationTheme(
       border: CutCornersBorder(),
       focusedBorder: CutCornersBorder(
         borderSide: BorderSide(
           width: 2.0,
-          color: kShrinePurple,
+          color: kShrineBrown900,
         ),
       ),
       floatingLabelStyle: TextStyle(
-        color: kShrinePurple,
+        color: kShrineBrown900,
       ),
     ),
   );
--- lib/mdc_files/home.dart
+++ lib/mdc_files/home.dart
@@ -20,7 +20,8 @@ import 'model/products_repository.dart';
 import 'supplemental/asymmetric_view.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  final Category category;
+  const HomePage({this.category = Category.all, Key? key}) : super(key: key);
 
   List<Card> _buildGridCards(BuildContext context) {
     List<Product> products = ProductsRepository.loadProducts(Category.all);
@@ -87,44 +88,47 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
-    // TODO: Pass Category variable to AsymmetricView (104)
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(
-            Icons.menu,
-            semanticLabel: 'menu',
-          ),
-          onPressed: () {},
-        ),
-        title: const Text('SHRINE'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(
-              Icons.search,
-              semanticLabel: 'search',
-            ),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(
-              Icons.tune,
-              semanticLabel: 'filter',
-            ),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      // body: GridView.count(
-      //   crossAxisCount: 2,
-      //   padding: const EdgeInsets.all(16.0),
-      //   childAspectRatio: 8.0 / 9.0,
-      //   children: _buildGridCards(context),
-      // ),
-      body: AsymmetricView(
-        products: ProductsRepository.loadProducts(Category.all),
-      ),
-      resizeToAvoidBottomInset: false,
+    return AsymmetricView(
+      products: ProductsRepository.loadProducts(category),
     );
+    // TODO: Pass Category variable to AsymmetricView (104)
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     leading: IconButton(
+    //       icon: const Icon(
+    //         Icons.menu,
+    //         semanticLabel: 'menu',
+    //       ),
+    //       onPressed: () {},
+    //     ),
+    //     title: const Text('SHRINE'),
+    //     actions: <Widget>[
+    //       IconButton(
+    //         icon: const Icon(
+    //           Icons.search,
+    //           semanticLabel: 'search',
+    //         ),
+    //         onPressed: () {},
+    //       ),
+    //       IconButton(
+    //         icon: const Icon(
+    //           Icons.tune,
+    //           semanticLabel: 'filter',
+    //         ),
+    //         onPressed: () {},
+    //       ),
+    //     ],
+    //   ),
+    //   // body: GridView.count(
+    //   //   crossAxisCount: 2,
+    //   //   padding: const EdgeInsets.all(16.0),
+    //   //   childAspectRatio: 8.0 / 9.0,
+    //   //   children: _buildGridCards(context),
+    //   // ),
+    //   body: AsymmetricView(
+    //     products: ProductsRepository.loadProducts(Category.all),
+    //   ),
+    //   resizeToAvoidBottomInset: false,
+    // );
   }
 }
